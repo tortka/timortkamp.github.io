bf9206e8823c0f1089a5fc805e761cef87f5bb20
tortka <tim.ortkamp@gmx.de>
2025-08-20 05:15:50 +0200

    Added RT25 leg 8, added blog pages (for newer and older posts), added text expander plugin to show/hide text

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$.expander.defaults.expandEffect = 'fadeIn';
+
+$(document).ready(function() {
+  // simple example, using all default options unless overridden globally
+  $('p.expandable').expander();
+});
